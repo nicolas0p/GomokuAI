24a38af0cb71646bd4da5d3391d93c65f3af83c7
nicolas0p <nicolas0pfeifer@gmail.com>
2017-08-28 17:43:23 -0300

    Created test MergingTwoSequences

--- test/tests.cpp
+++ test/tests.cpp
@@ -20,3 +20,12 @@ TEST(InsertMoveInBoard, TwoHorizontalNextMoves) {
 	board.insert_move({0,1}, Board::FIRSTPLAYER);
 	EXPECT_EQ(305, sum_sequence_values(board, Board::FIRSTPLAYER));
 }
+
+TEST(InsertMoveInBoard, MergingTwoSequences) {
+	int size = 15;
+	Board board(size, size);
+	board.insert_move({0,0}, Board::FIRSTPLAYER);
+	board.insert_move({0,2}, Board::FIRSTPLAYER);
+	board.insert_move({0,1}, Board::FIRSTPLAYER);
+	EXPECT_EQ(50008, sum_sequence_values(board, Board::FIRSTPLAYER));
+}
