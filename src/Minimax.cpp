86753347f59ca17b0f42e59828e579ea95d69883
nicolas0p <nicolas0pfeifer@gmail.com>
2017-09-03 22:22:04 -0300

    Passing depth 1 minimax tests

--- src/Minimax.cpp
+++ src/Minimax.cpp
@@ -11,19 +11,39 @@ Minimax::Minimax(const std::function<int (const Board &, const Board::Moves &)>&
 	_difficulty(difficulty)
 {}
 
+static const std::function<bool (const int&, const int&)> gt = std::greater<int>();
+static const std::function<bool (const int&, const int&)> ls = std::less<int>();
+
+std::function<bool (const int&, const int&)> get_other_compare(const std::function<bool (const int&, const int&)>& function)
+{
+	if(function.target<bool (const int&, const int&)>() == gt.target<bool (const int&, const int&)>()) {
+		return ls;
+	}
+	return gt;
+}
+
+Board::Moves get_other_player(const Board::Moves& player)
+{
+	if(player == Board::FIRSTPLAYER) {
+		return Board::SECONDPLAYER;
+	}
+	return Board::FIRSTPLAYER;
+}
+
 /*
  * */
 std::pair<int, int> Minimax::get_move(Board board)
 {
-	return std::get<0>(mmax(&board, _difficulty));
+	return std::get<0>(step(&board, _difficulty, std::greater<int>(), _player));
 }
 
-/* Max step of the minimax algorithm. It selects the maximum node of its children and chooses it
+/* Step of the minimax algorithm. It selects the maximum or minimum node of its children and chooses it
  * @param board the board from which it is desired to generate moves of this subtree
  * @param depth the depth that the algorithm should go
- * @returns a tuple containing the chosen move of this subtree, the value this move produces in the board, calculated by the heuristic, and its pruning alpha
+ * @param compare function to compare different moves in order to choose the best. Be it greater or lesser.
+ * @returns a tuple containing the chosen move of this subtree, the value this move produces in the board, calculated by the heuristic, and its pruning alpha or beta
  * */
-std::tuple<std::pair<int, int>, int, int> Minimax::mmax(Board * board, unsigned int depth)
+std::tuple<std::pair<int, int>, int, int> Minimax::step(Board * board, unsigned int depth, const std::function<bool (const int&, const int&)>& compare, const Board::Moves& player)
 {
 	//set of moves
 	auto moves = _generate_moves(*board);
@@ -32,14 +52,14 @@ std::tuple<std::pair<int, int>, int, int> Minimax::mmax(Board * board, unsigned 
 	//bottom of tree
 	if (depth <= 1) {
 		std::get<0>(choice) = *moves.begin();
-		board->insert_move(std::get<0>(choice), _player);
-		std::get<1>(choice) = _heuristic(*board, _player);
+		board->insert_move(std::get<0>(choice), player);
+		std::get<1>(choice) = _heuristic(*board, player);
 		board->remove_move(std::get<0>(choice));
 		moves.erase(moves.begin());
 		for(auto it : moves) {
-			board->insert_move(it,_player);
-			int current = _heuristic(*board, _player);
-			if(current > std::get<1>(choice)) {
+			board->insert_move(it, player);
+			int current = _heuristic(*board, player);
+			if(compare(current, std::get<1>(choice))) {
 				std::get<0>(choice) = it;
 				std::get<1>(choice) = current;
 			}
@@ -47,59 +67,14 @@ std::tuple<std::pair<int, int>, int, int> Minimax::mmax(Board * board, unsigned 
 		}
 		return choice;
 	}
-
-	board->insert_move(*moves.begin(),_player);
-	choice = mmin(board, depth - 1);
+	board->insert_move(*moves.begin(), player);
+	choice = step(board, depth - 1, get_other_compare(compare), get_other_player(player));
 	board->remove_move(*moves.begin());
 	moves.erase(moves.begin());
 	for(auto it : moves) {
-		board->insert_move(it,_player);
-		int current = std::get<1>(mmin(board, depth - 1));
-		if(current > std::get<1>(choice)) {
-			std::get<0>(choice) = it;
-			std::get<1>(choice) = current;
-		}
-		board->remove_move(it);
-	}
-	return choice;
-}
-
-/* Min step of the minimax algorithm. It selects the minimum node of its children and chooses it
- * @param board the board from which it is desired to generate moves of this subtree
- * @param depth the depth that the algorithm should go
- * @returns a tuple containing the chosen move of this subtree, the value this move produces in the board, calculated by the heuristic, and its pruning beta
- * */
-std::tuple<std::pair<int, int>, int, int> Minimax::mmin(Board * board, unsigned int depth)
-{
-	//set of moves
-	std::set<std::pair<int, int>> moves = _generate_moves(*board);
-	std::tuple<std::pair<int, int>, int, int> choice;
-	//bottom of tree
-	if (depth <= 1) {
-		std::get<0>(choice) = *moves.begin();
-		board->insert_move(std::get<0>(choice), _player);
-		std::get<1>(choice) = _heuristic(*board, _player);
-		board->remove_move(std::get<0>(choice));
-		moves.erase(moves.begin());
-		for(auto it : moves) {
-			board->insert_move(it, _player);
-			int current = _heuristic(*board, _player);
-			if(current < std::get<1>(choice)) {
-				std::get<0>(choice) = it;
-				std::get<1>(choice) = current;
-			}
-			board->remove_move(it);
-		}
-		return choice;
-	}
-	board->insert_move(*moves.begin(), _player);
-	choice = mmin(board, depth - 1);
-	board->remove_move(*moves.begin());
-	moves.erase(moves.begin());
-	for(auto it : moves) {
-		board->insert_move(it, _player);
-		int current = std::get<1>(mmin(board, depth - 1));
-		if(current < std::get<1>(choice)) {
+		board->insert_move(it, player);
+		int current = std::get<1>(step(board, depth - 1, get_other_compare(compare), get_other_player(player)));
+		if(compare(current, std::get<1>(choice))) {
 			std::get<0>(choice) = it;
 			std::get<1>(choice) = current;
 		}
